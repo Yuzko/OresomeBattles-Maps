4575b1dfd053d6b9ceff455adf00881cc64418a7
Yuzko <ow_oh_kiss_liss3456@hotmail.com>
2013-08-18 20:48:25 +1000

    Update Template.java

--- Template.java
+++ Template.java
@@ -7,31 +7,25 @@ import org.bukkit.inventory.*;
 import com.oresomecraft.BattleMaps.*;
 import com.oresomecraft.OresomeBattles.api.*;
 
-public class Template extends BattleMap implements IBattleMap, Listener {
+public class BiomeBattle extends BattleMap implements IBattleMap, Listener {
 
-    public Template() {
+    public BiomeBattle() {
         super.initiate(this);
         setDetails(name, fullName, creators, modes);
     }
 
     // Map details
-    String name = "template";
-    String fullName = "Template";
-    String creators = "derp, herp and harpaderp";
-    Gamemode[] modes = {Gamemode.TDM, Gamemode.FFA, Gamemode.INFECTION, Gamemode.CTF, Gamemode.KOTH};
+    String name = "biomebattle";
+    String fullName = "BiomeBattle";
+    String creators = "SuperDuckFace, Evil_Emo and Yuzko";
+    Gamemode[] modes = {Gamoemode.FFA, Gamemode.KOTH};
 
     public void readyTDMSpawns() {
         World w = Bukkit.getServer().getWorld(name);
 
-        redSpawns.add(new Location(w, 0, 99, 27, 2, 0));
-        redSpawns.add(new Location(w, -9, 110, -20, 0, 0));
-        redSpawns.add(new Location(w, 21, 105, -13, 0, 0));
-        redSpawns.add(new Location(w, 4, 106, -41, 0, 0));
+        redSpawns.add(new Location(w, -61, 73, -54));
 
-        blueSpawns.add(new Location(w, -9, 110, -20, 0, 0));
-        blueSpawns.add(new Location(w, 0, 99, 27, 0, 0));
-        blueSpawns.add(new Location(w, -16, 108, -3, 0, 0));
-        blueSpawns.add(new Location(w, -30, 108, -3, 0, 0));
+        blueSpawns.add(new Location(w, 48, 72, 42));
 
         setRedSpawns(name, redSpawns);
         setBlueSpawns(name, blueSpawns);
@@ -40,8 +34,18 @@ public class Template extends BattleMap implements IBattleMap, Listener {
     public void readyFFASpawns() {
         World w = Bukkit.getServer().getWorld(name);
 
-        FFASpawns.add(new Location(w, 0, 99, 27, 2, 0));
-        FFASpawns.add(new Location(w, -9, 110, -20, 0, 0));
+        FFASpawns.add(new Location(w, -61, 73, -54));
+        FFASpawns.add(new Location(w, 48, 72, 42));
+        FFASpawns.add(new location(w, -69, 72, 63));
+        FFASpawns.add(new location(w, -64, 70, 12));
+        FFASpawns.add(new location(w, -51, 87, 10));
+        FFASpawns.add(new location(w, -47, 80, -41));
+        FFASpawns.add(new location(w, -17, 70, -84));
+        FFASpawns.add(new location(w, 83, 78, -22));
+        FFASpawns.add(new location(w, 13, 69, -3));
+        FFASpawns.add(new location(w, 15, 74, 40));
+        FFASpawns.add(new location(w, -83, 74, -7));
+        
 
         setFFASpawns(name, FFASpawns);
     }
@@ -56,6 +60,11 @@ public class Template extends BattleMap implements IBattleMap, Listener {
         ItemStack IRON_BOOTS = new ItemStack(Material.IRON_BOOTS, 1);
         ItemStack IRON_SWORD = new ItemStack(Material.IRON_SWORD, 1);
 
+        p.getInventory().setLeggings(LEATHER_PANTS);
+        p.getInventory().setBoots(LEATHER_BOOTS);
+        p.getInventory().setChestplate(IRON_CHESTPLACE);
+        p.getInventory().setHelmet(LEATHER_CAP);
+
         p.getInventory().setBoots(IRON_BOOTS);
         p.getInventory().setLeggings(IRON_PANTS);
         p.getInventory().setChestplate(IRON_CHESTPLATE);
@@ -68,20 +77,19 @@ public class Template extends BattleMap implements IBattleMap, Listener {
         p.setItem(1, Material.BOW, 1);
         p.setItem(2, Material.COOKED_BEEF, 1);
         p.setItem(4, Material.ARROW, 64);
-        p.setItem(5, Material.EXP_BOTTLE, 5);
-
+        p.setItem(5, Material.ARROW, 64);
 
     }
 
     // Region. (Top corner block and bottom corner block.
     // Top left corner.
-    public int x1 = -100;
-    public int y1 = 160;
-    public int z1 = -70;
+    public int x1 = -122;
+    public int y1 = 209;
+    public int z1 = -125;
 
     //Bottom right corner.
-    public int x2 = -70;
-    public int y2 = 30;
-    public int z2 = 50;
+    public int x2 = 118;
+    public int y2 = -38;
+    public int z2 = 109;
 
 }
